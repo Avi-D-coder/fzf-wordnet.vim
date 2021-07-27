9098882553e73a9e9a58989ca4cfbfe0240560a9
Matthew Bartllet <endercreeper92@gmail.com>
2021-07-27 13:30:46 -0400

    Revert "testing useing /usr/share/dict/words"
    This reverts commit 9c474c4d5c06366c62ec7c852444c314726d7248.

---
+++ words/extract-wordnet.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ -z $1 ] || [ -z $2 ]; then
+    echo 'Useage: `extract-wordnet.sh [wordnet/dict directory] [output words file]`'
+    exit
+fi
+
+dir=$(realpath $1)
+
+# Derived from balta2ar's comments on https://ddrscott.github.io/blog/2017/fzf-dictionary/
+echo 'collecting from data files'
+tail -n +30 $dir/data.* | cut -f5 -d' ' | sed -e 's/_/ /g' >>$2
+
+echo 'collecting from index files'
+tail -n +30 $dir/index.[anv]* | cut -f1 -d' ' | sed -e 's/_/ /g' >>$2
+
+echo 'sorting'
+sort -u -o $2 $2
